6747be465324c895f04ea62509f6ab1deeb51ca0
Victor Manoel <victormanoel7895123@hotmail.com>
2021-05-01 20:59:01 -0300

    update js

---
+++ scripts.js
@@ -0,0 +1,12 @@
+/* function multiplication (numero){
+    return numero*5;
+};
+let valorTotal = multiplication(10);
+console.log(valorTotal); */
+
+
+
+//Operador Ternário
+//let ponto = 150;
+//let tipo = ponto > 50 ? "aprovado" : "reprovado";
+//console.log(tipo);
