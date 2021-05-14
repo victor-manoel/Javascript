45a777d5202cd2e86dc97a836d00b20dd6ad18e4
Victor Manoel <victormanoel7895123@hotmail.com>
2021-05-13 23:59:30 -0300

    up

--- If_else.js
+++ If_else.js
@@ -1,8 +1,8 @@
 let hora = 12;
 
-if (hora>6 $$ hora<12){
+if (hora > 6 && hora < 13){
     console.log('Bom dia')
-} else if {
+} else if (hora > 12 && hora < 19){
     console.log('Boa tarde')
 } else {
     console.log('Boa noite')
